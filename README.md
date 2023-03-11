5b5ec259fd65d85d20ed5890a7f4f7ad349609ea
mrkchu <38389212+mrkchu@users.noreply.github.com>
2023-03-11 10:42:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-3uhya6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-3uhya6)
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <h1>Hello from aa bb cc {{name}}!</h1>
+    <a target="_blank" href="https://angular.io/start">
+      Learn more about Angulaa - testing
+    </a>
+  `,
+})
+export class App {
+  name = 'Angular';
+}
+
+bootstrapApplication(App);
